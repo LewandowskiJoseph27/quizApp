533028453dbc2e45cdce0c8fc43cdb13eb5590c0
Joe Lewandowski <joseph.c.lewandowski@gmail.com>
2025-05-13 18:00:06 -0700

    question class

--- question.h
+++ question.h
@@ -3,5 +3,17 @@
 
 #include <string>
 
+class Question {
+    public: 
+        Question(const std::string& questionText, const std::string& answerText);
+        void askQuestion() const;
+        bool checkAnswer(const std::string& userAnswer) const;
+        
+    private:
+        int number;
+        std::string questionText;
+        std::string answerText;
+        bool asked;
+}
 
 #endif // QUESTION_H
